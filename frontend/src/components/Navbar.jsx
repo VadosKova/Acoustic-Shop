f21837186bd3f335881cf3d23cc4a55cbaba0ac8
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-03-22 18:27:01 +0200

    Updated style for product adding and navbar for admin

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 
 export default function Navbar({ cartCount = 0 }) {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const isAdmin = user?.email === "admin@gmail.com";
 
   return (
     <nav>
       <ul className="nav-menu">
         <li><Link to="/" className="nav-link">Home</Link></li>
-        <li><Link to="/cart" className="nav-link">Cart ({cartCount})</Link></li>
+        <li>{!isAdmin && (<Link to="/cart" className="nav-link">Cart ({cartCount})</Link>)}</li>
         <li><Link to="/profile" className="nav-link">Profile</Link></li>
       </ul>
     </nav>
